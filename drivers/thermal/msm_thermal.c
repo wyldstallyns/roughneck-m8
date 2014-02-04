a4ca0b4292a3f0e46a7f04954a010d9c8dae4594
Steve Kondik <shade@chemlab.org>
2014-02-04 02:12:53 -0800

    msm: Fix high load average from uninterruptible waits
     * The load average on all MSMs is currently hovering at around 14
       due to uninterruptible waits in various places.
     * Convert these waits to interruptible waits to bring the load
       back down to zero at idle.
    
    Change-Id: I14bad4af135b1594e235e2252cb484cb34d91054
    
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: massstash <massstash@gmail.com>
    
    Conflicts:
    	drivers/thermal/msm_thermal.c

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -1072,9 +1072,7 @@ static __ref int do_hotplug(void *data)
 	}
 
 	while (!kthread_should_stop()) {
-		while (wait_for_completion_interruptible(
-			&hotplug_notify_complete) != 0)
-			;
+		while (wait_for_completion_interruptible(&hotplug_notify_complete) != 0);
 		INIT_COMPLETION(hotplug_notify_complete);
 		mask = 0;
 
@@ -1512,9 +1510,7 @@ static __ref int do_freq_mitigation(void *data)
 	uint32_t cpu = 0, max_freq_req = 0, min_freq_req = 0;
 
 	while (!kthread_should_stop()) {
-		while (wait_for_completion_interruptible(
-			&freq_mitigation_complete) != 0)
-			;
+		while (wait_for_completion_interruptible(&freq_mitigation_complete) != 0);
 		INIT_COMPLETION(freq_mitigation_complete);
 
 		for_each_possible_cpu(cpu) {
