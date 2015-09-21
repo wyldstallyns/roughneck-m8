b966d3ea3c37f2f81b13ef553f3bbb1e435358c0
TotallyAnxious <jentrefzger@gmail.com>
2015-09-21 06:08:15 -0400

    Update touchboost.h

--- include/linux/touchboost.h
+++ include/linux/touchboost.h
@@ -1,8 +1,8 @@
 /*
- * linux/include/linux/touchboost.h
+ *  linux/include/linux/touchboost.h
  *
  * franciscofranco.1990@gmail.com
- *
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
@@ -10,9 +10,6 @@
 #ifndef _LINUX_TOUCHBOOST_H
 #define _LINUX_TOUCHBOOST_H
 
-extern int input_boost_freq;
-
 u64 get_input_time(void);
-int get_input_boost_duration(void);
 
 #endif
