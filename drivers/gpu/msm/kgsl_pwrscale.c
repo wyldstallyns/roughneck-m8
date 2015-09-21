df8f25ca9ed172a33bddda7de2cc2fb29d4119b1
TotallyAnxious <jentrefzger@gmail.com>
2015-09-20 20:19:24 -0400

    Update kgsl_pwrscale.c

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -320,7 +320,7 @@ int kgsl_pwrscale_init(struct device *dev, const char *governor)
 	for (i = 0; i < (pwr->num_pwrlevels - 1); i++)
 		pwrscale->freq_table[out++] = pwr->pwrlevels[i].gpu_freq;
 
-	profile->max_state = out;
+	profile->max_state = pwr->num_pwrlevels - 1;
 	
 	profile->freq_table = pwrscale->freq_table;
 
